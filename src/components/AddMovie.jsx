024ab7c1c796ca57aefdd36188862f6a70958f0d
Sagya Ghimire <np03cs4s250084@heraldcollege.edu.np>
2026-06-27 17:57:17 +0545

    Added Navbar with Browsing, seeing Watchlist and Adding movies and also modified their css

---
+++ src/components/AddMovie.jsx
@@ -0,0 +1,8 @@
+<a href="#" className="hover:text-yellow-400">
+          Watchlist
+        </a>
+
+        <a href="#" className="hover:text-yellow-400">
+          Add Movie
+        </a>
+      </div>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,25 @@
 function NavBar() {
   return (
     <nav>
-      <h1 className="text-2xl text-gray-800 font-bold text-center">Movie Database</h1>
+      <h1 className="text-2xl font-bold text-gray-800 text-center">Movie Database</h1>
+
+      <div className="flex gap-6 justify-center space-x-4 mt-4 text-gray-800">
+        <a href="#" className="font-semibold text-gray-700 font-medium hover:text-stone-500 transition-colors">
+          Browse
+        </a>
+        
+        <a href="#" className="font-semibold text-gray-700 font-medium hover:text-stone-500 transition-colors">
+          Watchlist
+        </a>
+
+        <a href="#" className="font-semibold text-gray-700 font-medium hover:text-stone-500 transition-colors">
+          Add Movie
+        </a>
+      </div>
+
     </nav>
   );
 }
 
 export default NavBar;
+
