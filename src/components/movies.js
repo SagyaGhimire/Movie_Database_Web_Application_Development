27083a7086add03c5fdfe68a4e5808eed3e70bcb
Sagya Ghimire <np03cs4s250084@heraldcollege.edu.np>
2026-07-06 09:37:22 +0545

    Updated the movies.js file with more details of the movies and then made sure that when we click on the movie the details can be previewed

--- src/components/movies.js
+++ src/components/movies.js
@@ -1,5 +1,5 @@
+/* This contains the list of movies with their details like title, genre, year, rating, director, synopsis and cast */
 
-{/*This contains the list of movies with their details like title, genre, year and rating*/}
 const movies = [
   {
     id: 1,
@@ -7,34 +7,83 @@ const movies = [
     genre: "Comedy/Drama",
     year: 2009,
     rating: 9.0,
+    director: "Rajkumar Hirani",
+    synopsis:
+      "Two friends search for their long-lost college companion while remembering the unforgettable lessons he taught them about life, education, and following their dreams.",
+    cast: [
+      "Aamir Khan",
+      "R. Madhavan",
+      "Sharman Joshi",
+      "Kareena Kapoor",
+    ],
   },
+
   {
     id: 2,
     title: "Dangal",
     genre: "Sports/Drama",
     year: 2016,
     rating: 8.4,
+    director: "Nitesh Tiwari",
+    synopsis:
+      "A former wrestler trains his daughters to become world-class wrestlers despite facing social challenges and criticism.",
+    cast: [
+      "Aamir Khan",
+      "Fatima Sana Shaikh",
+      "Sanya Malhotra",
+      "Sakshi Tanwar",
+    ],
   },
+
   {
     id: 3,
     title: "Bhool Bhulaiyaa 2",
     genre: "Comedy/Horror",
     year: 2022,
     rating: 7.2,
+    director: "Anees Bazmee",
+    synopsis:
+      "A young man gets caught in mysterious events surrounding an old mansion believed to be haunted by an angry spirit.",
+    cast: [
+      "Kartik Aaryan",
+      "Kiara Advani",
+      "Tabu",
+      "Rajpal Yadav",
+    ],
   },
+
   {
     id: 4,
     title: "Housefull 4",
     genre: "Comedy",
     year: 2019,
     rating: 5.8,
+    director: "Farhad Samji",
+    synopsis:
+      "Three couples discover they were reincarnated from the 15th century and try to reunite with their true soulmates.",
+    cast: [
+      "Akshay Kumar",
+      "Riteish Deshmukh",
+      "Bobby Deol",
+      "Kriti Sanon",
+    ],
   },
+
   {
     id: 5,
     title: "Adipurush",
     genre: "Action/Fantasy",
     year: 2023,
     rating: 3.8,
+    director: "Om Raut",
+    synopsis:
+      "A modern retelling of the Ramayana that follows Raghava's mission to rescue his wife from the demon king Lankesh.",
+    cast: [
+      "Prabhas",
+      "Kriti Sanon",
+      "Saif Ali Khan",
+      "Sunny Singh",
+    ],
   },
 ];
 
