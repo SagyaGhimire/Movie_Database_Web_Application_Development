e203093add0c7fb5c4357122480f374cd1bd972f
Sagya Ghimire <np03cs4s250084@heraldcollege.edu.np>
2026-07-13 09:42:53 +0545

    Movie model file made to store movie information

---
+++ backend/src/models/MovieModel.js
@@ -0,0 +1,7 @@
+import movies from "../../data/movies.js";
+
+// This array stores movies added to watchlist
+let watchlist = [];
+
+// Export both movies and watchlist
+export { movies, watchlist };
