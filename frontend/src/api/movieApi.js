6a956134e547bfee02a20a535e16270554b3002d
Sagya Ghimire <np03cs4s250084@heraldcollege.edu.np>
2026-08-03 10:54:21 +0545

    error 401 tried to fix

--- frontend/src/api/movieApi.js
+++ frontend/src/api/movieApi.js
@@ -7,6 +7,27 @@ const api = axios.create({
     },
 });
 
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+        config.headers = config.headers || {};
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+});
+
+api.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        if (error.response?.status === 401) {
+            localStorage.removeItem('token');
+            localStorage.removeItem('user');
+            delete api.defaults.headers.common.Authorization;
+        }
+        return Promise.reject(error);
+    }
+);
+
 export function setAuthToken(token) {
     if (token) {
         api.defaults.headers.common.Authorization = `Bearer ${token}`;
